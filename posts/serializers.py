090a72946d3d15c40b3649bbb6632f4f4da16e0c
Moon Geonwoo <moongunwoo7019@gmail.com>
2024-08-01 17:24:53 +0900

    add: post posts/

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
 from .models import Post
+from users.serializers import UserTinySerializer
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    writer = UserTinySerializer()
+
     class Meta:
         model = Post
         fields = [
             "id",
             "writer",
             "title",
+            "created_at",
+        ]
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    writer = UserTinySerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "detail",
+            "writer",
         ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import PostListSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import PostListSerializer, PostCreateSerializer
 from .models import Post
 
 
 class PostListAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         post = Post.objects.all()
         serializer = PostListSerializer(post, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(writer=request.user)
+            serializer = PostCreateSerializer(post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
